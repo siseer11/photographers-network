b7e837135f3548d9fc5b05f7c34d65c83f0ab6ca
Bianca <bianca.hoellmueller@gmx.net>
2018-10-03 16:31:09 +0200

    cleaned up code from profile

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import LoadingPage from "../components/LoadingPage";
-import GbNavBar from '../components/gbNav';
-import { ProfileCard } from "../components/ProfileCard";
-import { LinkLists } from "../components/LinkLists";
+// dependencies
+import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom';
+import fire from '../config/Fire';
+
+// components
+import {ProfileCard} from "../components/ProfileCard";
+import {LinkLists} from "../components/LinkLists";
+import {GbFooter} from "../components/Footer";
+import {FacebookSVG} from "../components/svg/FacebookSVG";
+import {TwitterSVG} from "../components/svg/TwitterSVG";
+import {InstagramSVG} from "../components/svg/InstagramSVG";
+
+// containers
 import PhotographerContent from "./PhotographerContent";
 import CompanyContent from "./CompanyContent";
-import fire from '../config/Fire';
+import LoadingPage from "../components/LoadingPage";
+import GbNavBar from '../components/gbNav';
+
 
 export default class Profile extends Component {
     state = {
@@ -37,36 +47,32 @@ export default class Profile extends Component {
      */
     fetchUserInformation = () => {
         const {uid} = this.state;
-        let userData = {};
         this.database.child('users').child(uid).once('value')
             .then(snap => {
                 let data = snap.val();
-                userData = {
+                let userData = {
                     displayName: data.displayName,
                     email: data.email,
                     photoURL: data.photoURL,
+                    type: data.type,
                 };
-                this.setState({userData: userData, fetchedUserData: true}, () => {
-                    console.log("loaded:" + this.state.fetchedUserData);
-                });
+                this.setState({userData: userData, fetchedUserData: true});
             });
     };
 
     render() {
-        const {user, type, loadedResponse} = this.props;
-        const {fetchedUserData, userData, uid} = this.state;
+        const {user, loadedResponse} = this.props;
+        const {fetchedUserData, userData, uid, pageLinks} = this.state;
 
         let otherUser = true;
         let loaded = false;
         let currUser = null;
 
+        // looks if there is response from the current user
+        // and the user data has been already fetched
         if (loadedResponse && fetchedUserData) {
-            if (user.uid === uid || uid === '') {
-                otherUser = false;
-                currUser = user;
-            } else {
-                currUser = userData;
-            }
+            otherUser = user.uid !== uid;
+            currUser = userData;
             loaded = true;
             console.log("loaded everything!");
         }
@@ -77,92 +83,67 @@ export default class Profile extends Component {
                     loaded ?
                         (user ?
                             (
-                                <div className='profile'>
-                                    <GbNavBar
-                                        righLinks={
-                                            [{txt: 'Sign out', clickHandler: this.logout}]
-                                        }
-                                        loggedIn={false}
-                                    />
-                                    <ProfileCard
-                                        backgroundImg='https://images.unsplash.com/photo-1526080676457-4544bf0ebba9?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=981026b7c3ee99d54e0811e984995340'
-                                        profileImg="https://images.unsplash.com/photo-1520466809213-7b9a56adcd45?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=6dd9dc582c677370d110940fda65b992"
-                                        type={type}
-                                    >
-                                        {currUser.displayName}
-                                    </ProfileCard>
-                                    <div className="profile-content">
-                                        <LinkLists links={this.state.pageLinks}
-                                                   txtClasses="gb-text-black-opacity-30 gb-subtitle-medium"
-                                                   liClasses="footer-nav-item"/>
-                                    </div>
-                                    {otherUser ? (<div>Not your own profile</div>) : (
-                                        type === "photographer" ? (<PhotographerContent/>) : (<CompanyContent/>)
-                                    )}
-
-                                    <GbFooter
-                                        socialMedias={[
-                                            {
-                                                icon: <InstagramSVG
-                                                    classes='gb-icon-fill-black-opacity-30 gb-icon-small'/>, link: '#'
-                                            },
-                                            {
-                                                icon: <TwitterSVG
-                                                    classes='gb-icon-fill-black-opacity-30 gb-icon-small'/>,
-                                                link: '#'
-                                            },
-                                            {
-                                                icon: <FacebookSVG
-                                                    classes='gb-icon-fill-black-opacity-30 gb-icon-small'/>,
-                                                link: '#'
-                                            }]}
-                                    />
-                                </div>) :
+                                <ProfileView user={currUser}
+                                             isOtherUser={otherUser}
+                                             logoutHandler={this.logout}
+                                             pageLinks={pageLinks}
+                                             uid={uid}
+                                />
+                            ) :
                             (<Redirect to="/"/>)) : (<LoadingPage/>)
                 }
+            </React.Fragment>
         );
     }
 }
 
-/*
-<ProfileView {...this.props} logoutHandler={this.logout} pageLinks={pageLinks} uid={uid} />
+const ProfileView = ({isOtherUser, user, logoutHandler, pageLinks}) => (
+    <div className='profile'>
+        <GbNavBar
+            righLinks={
+                [{txt: 'Sign out', clickHandler: this.logout}]
+            }
+            loggedIn={false}
+        />
 
+        <ProfileCard
+            backgroundImg='https://images.unsplash.com/photo-1526080676457-4544bf0ebba9?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=981026b7c3ee99d54e0811e984995340'
+            profileImg="https://images.unsplash.com/photo-1520466809213-7b9a56adcd45?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=6dd9dc582c677370d110940fda65b992"
+            type={user.type}
+        >
+            {user.displayName}
+        </ProfileCard>
 
-const ProfileView = ({ type, user, logoutHandler, pageLinks, uid }) => (
-	<div className='profile'>
-		<GbNavBar
-			righLinks={
-				[{ txt: 'Sign out', clickHandler: logoutHandler }]
-			}
-			loggedIn={false}
-		/>
-		<ProfileCard
-			backgroundImg='https://images.unsplash.com/photo-1526080676457-4544bf0ebba9?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=981026b7c3ee99d54e0811e984995340'
-			profileImg="https://images.unsplash.com/photo-1520466809213-7b9a56adcd45?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=6dd9dc582c677370d110940fda65b992"
-			type={type}
-		>
-			{user.displayName}
-		</ProfileCard>
+        <div className="profile-content">
+            <LinkLists links={this.state.pageLinks}
+                       txtClasses="gb-text-black-opacity-30 gb-subtitle-medium"
+                       liClasses="footer-nav-item"/>
+        </div>
 
-		<div className="profile-content">
-			<LinkLists links={pageLinks}
-				txtClasses="gb-text-black-opacity-30 gb-subtitle-medium"
-				liClasses="footer-nav-item" />
-		</div>
+        {isOtherUser ?
+            (<div>Not your own profile</div>) :
+            (type === "photographer" ?
+                    (<PhotographerContent/>) :
+                    (<CompanyContent/>)
+            )
+        }
 
-	</div>
-)
-
-*/
-/*
-		{uid ? (
-			<div>Not your own profile</div>
-		) : (
-				type === "photographer" ? (
-					<PhotographerContent />
-				) : (
-						<CompanyContent />
-					)
-			)
-		}
-		*/
+        <GbFooter
+            socialMedias={[
+                {
+                    icon: <InstagramSVG
+                        classes='gb-icon-fill-black-opacity-30 gb-icon-small'/>, link: '#'
+                },
+                {
+                    icon: <TwitterSVG
+                        classes='gb-icon-fill-black-opacity-30 gb-icon-small'/>,
+                    link: '#'
+                },
+                {
+                    icon: <FacebookSVG
+                        classes='gb-icon-fill-black-opacity-30 gb-icon-small'/>,
+                    link: '#'
+                }]}
+        />
+    </div>
+);
