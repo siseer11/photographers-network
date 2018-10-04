36d717eefb7ea5f256fd8ce55d2b57b715f82aa1
Bianca <bianca.hoellmueller@gmx.net>
2018-10-04 09:33:15 +0200

    indent line space to 2

--- src/components/PhotographerCard.js
+++ src/components/PhotographerCard.js
@@ -3,13 +3,13 @@ import {Link} from 'react-router-dom';
 import {Avatar} from "./Avatar";
 
 export const PhotographerCard = ({uid, userPic, userName, userLocation}) => {
-    let path = `/profile/${uid}`;
-    return (
-        <div className="user-card">
-            <Avatar userImageUrl={userPic}/>
-            <h2>{userName}</h2>
-            <h5>{userLocation}</h5>
-            <Link to={path} className="gb-btn gb-btn-small gb-btn-primary">View profile</Link>
-        </div>
-    )
+  let path = `/profile/${uid}`;
+  return (
+    <div className="user-card">
+      <Avatar userImageUrl={userPic}/>
+      <h2>{userName}</h2>
+      <h5>{userLocation}</h5>
+      <Link to={path} className="gb-btn gb-btn-small gb-btn-primary">View profile</Link>
+    </div>
+  )
 };
--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -5,55 +5,55 @@ import GbNavBar from '../components/gbNav';
 import fire from '../config/Fire';
 
 export default class Dashboard extends Component {
-	state = {
-		pageLinks: [
-			{ txt: "Facebook", link: "www.facebook.com" },
-			{ txt: "Twitter", link: "www.twitter.com" }
-		]
-	};
+  state = {
+    pageLinks: [
+      {txt: "Facebook", link: "www.facebook.com"},
+      {txt: "Twitter", link: "www.twitter.com"}
+    ]
+  };
 
-	/**
-		* Logs out the user and redirects him to home.
-		*/
-	logout = () => {
-		fire.auth().signOut();
-		this.props.history.push("/");
-	};
+  /**
+   * Logs out the user and redirects him to home.
+   */
+  logout = () => {
+    fire.auth().signOut();
+    this.props.history.push("/");
+  };
 
-	render() {
-		const { authentificated, type, loading } = this.props;
-		// checks, if there is already a response of the database
-		// if not, shows the loading page
-		// if yes, checks, if there is actually a user (to avoid to get to the dashboard
-		// by just typing dashboard into the url), if there's none, redirects to home
-		return (
-			<React.Fragment>
-				{
-					loading ? (
-						<LoadingPage />
-					):(
-						authentificated ?(
-							<DashboardView {...this.props} logoutHandler={this.logout}/>
-						):(
-							<Redirect to="/signIn" />
-						)
-					)
-				}
-			</React.Fragment>
-		);
-	}
+  render() {
+    const {authentificated, type, loading} = this.props;
+    // checks, if there is already a response of the database
+    // if not, shows the loading page
+    // if yes, checks, if there is actually a user (to avoid to get to the dashboard
+    // by just typing dashboard into the url), if there's none, redirects to home
+    return (
+      <React.Fragment>
+        {
+          loading ? (
+            <LoadingPage/>
+          ) : (
+            authentificated ? (
+              <DashboardView {...this.props} logoutHandler={this.logout}/>
+            ) : (
+              <Redirect to="/signIn"/>
+            )
+          )
+        }
+      </React.Fragment>
+    );
+  }
 }
 
-const DashboardView = ({type , user , logoutHandler}) => (
-	<div className='dashboard' style={{backgroundColor : 'rgba(0,0,0,.5)' , width: '100vw' , height : '100vh'}}>
-			<GbNavBar
-				righLinks={
-					[{ txt: 'Sign out', clickHandler: logoutHandler }]
-				}
-				loggedIn={false}
-			/>
-			<h2 style={{paddingTop : 150}}>The user is a {type} , name : {user.displayName}</h2>
-      <Link to={profilePath} className="gb-btn gb-btn-small gb-btn-primary">Userprofile</Link>
-      <Link to='/search-photographers' className="gb-btn gb-btn-small gb-btn-primary">Search photographers</Link>
-	</div>
+const DashboardView = ({type, user, logoutHandler}) => (
+  <div className='dashboard' style={{backgroundColor: 'rgba(0,0,0,.5)', width: '100vw', height: '100vh'}}>
+    <GbNavBar
+      righLinks={
+        [{txt: 'Sign out', clickHandler: logoutHandler}]
+      }
+      loggedIn={false}
+    />
+    <h2 style={{paddingTop: 150}}>The user is a {type} , name : {user.displayName}</h2>
+    <Link to={profilePath} className="gb-btn gb-btn-small gb-btn-primary">Userprofile</Link>
+    <Link to='/search-photographers' className="gb-btn gb-btn-small gb-btn-primary">Search photographers</Link>
+  </div>
 )
