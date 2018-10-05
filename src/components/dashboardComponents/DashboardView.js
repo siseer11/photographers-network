86f9a02d9c826b8427b13828b73d63d9d5dae22b
Bianca <bianca.hoellmueller@gmx.net>
2018-10-05 14:44:37 +0200

    enabled the apply function, fixed some bugs on the dashboard

--- src/components/dashboardComponents/DashboardView.js
+++ src/components/dashboardComponents/DashboardView.js
@@ -1,19 +1,46 @@
 // dependencies
-import React, {Component} from "react";
+import React from "react";
+import {Link} from "react-router-dom";
 
 // components
 import {DashboardHeader} from "./DashboardHeader";
 import {NavFooterWrapper} from "../../containers/NavFooterWrapper";
 
+// containers
+import MyJobOffers from '../../containers/MyJobOffers';
+import AppliedJobs from "../../containers/AppliedJobs";
 
-const DashboardView = ({user, type, linkHandler, activeComponent, headerLinks, logoutHandler}) => {
+
+const DashboardView = ({user, type, linkHandler, activeComponent, headerLinks, loading}) => {
+
+  let currentComponent = '';
+
+  //TODO: maybe find a better solultion than the switch?
+  switch (activeComponent) {
+    case "Home":
+      currentComponent = type === "photographer" ?
+        (<Link to='/jobs' className="gb-btn gb-btn-medium gb-btn-primary">Search for jobs</Link>) :
+        (<Link to='../createJob' className="gb-btn gb-btn-medium gb-btn-primary">Create job offer</Link>)
+      ;
+      break;
+    case "Applied Jobs":
+      currentComponent = (<AppliedJobs user={user} loading={loading}/>);
+      break;
+    case "My Jobs":
+      currentComponent = (<MyJobOffers user={user} loading={loading} />);
+      break;
+    default:
+      currentComponent = (<div>No fitting component!</div>);
+      break;
+  }
+
   return (
     <div>
       <DashboardHeader type={type} links={headerLinks} uid={user.uid}
                        linkHandler={linkHandler}>
         Welcome {user.displayName}!
       </DashboardHeader>
-      {activeComponent}
+      {currentComponent}
     </div>
   );
 };
