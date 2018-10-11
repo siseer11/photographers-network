2fcec36c881fed13f2d2d6043264ed302358df74
biancaaaaa <32326182+biancaaaaa@users.noreply.github.com>
2018-10-11 15:35:05 +0200

    Delete Fire.js

--- public/scripts/config/Fire.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  apiKey: "AIzaSyDz-3onacsNbODFZpgBVnrKad0T54g67Ew",
-  authDomain: "photographers-network-5d97b.firebaseapp.com",
-  databaseURL: "https://photographers-network-5d97b.firebaseio.com",
-  projectId: "photographers-network-5d97b",
-  storageBucket: "photographers-network-5d97b.appspot.com",
-  messagingSenderId: "922716641909"
-};
-firebase.initializeApp(config);
