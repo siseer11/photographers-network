acbbae39451542f1581c26ae7e2184c156fd03e3
Cuciureanu Dragos <dragos_bv11@yahoo.com>
2018-10-30 16:03:20 +0100

    added the ProfileEdit

--- src/components/my-jobs/MyJobsCategoryView.js
+++ src/components/my-jobs/MyJobsCategoryView.js
@@ -1,38 +1,51 @@
 // dependencies
 import React from "react";
+import WithModal from "../../RenderProp/WithModal";
 
 // components
-import {GbCard50} from '../gbCard50';
-import {ArrowDown} from "../svg/ArrowDown";
+import { GbCard50 } from "../gbCard50";
+import { ArrowDown } from "../svg/ArrowDown";
 
-export const MyJobsCategoryView = ({categoryTitle, jobs}) => (
-  <div className="category">
-    <div className="title-bar">
-      <h2>{categoryTitle}</h2>
-      <ArrowDown classes={`gb-icon-fill-black-opacity-30 gb-icon-small`}/>
-    </div>
-    {jobs.length > 0 ?
-      (<div>
-        {
-          jobs.map(el => (
-            <GbCard50
-              key={el.jobbId}
-              cardLink={`${categoryTitle === "Open" ? "open" : "progress"}-job/${el.jobbId}`}
-              type='half-left'
-              source={{
-                txt: el.companyName,
-                link: `/profile/${el.companyId}`
-              }}
-              postedTime={new Date(el.date).toLocaleDateString("en-US")}
-              category={el.type}
-            >
-              {el.title}
-            </GbCard50>
-          ))
-        }
-      </div>) :
-      <div className="no-jobs">No jobs in this category.</div>
-    }
-
-  </div>
+export const MyJobsCategoryView = ({ categoryTitle, jobs }) => (
+  <WithModal closeItemClass="close-acordeon">
+    {({ showModal }) => (
+      <div className="category">
+        <div className="title-bar close-acordeon">
+          <h2>{categoryTitle}</h2>
+          <ArrowDown
+            style={{ opacity: showModal ? 0.5 : 1 }}
+            classes={`gb-icon-fill-black-opacity-30 gb-icon-small close-acordeon`}
+          />
+        </div>
+        {jobs.length > 0 ? (
+          <div className={showModal ? "not-expanded" : "expanded"}>
+            {jobs.map(el => (
+              <GbCard50
+                key={el.jobbId}
+                cardLink={`${
+                  categoryTitle === "Open" ? "open" : "progress"
+                }-job/${el.jobbId}`}
+                type="half-left"
+                source={{
+                  txt: el.companyName,
+                  link: `/profile/${el.companyId}`
+                }}
+                postedTime={new Date(el.date).toLocaleDateString("en-US")}
+                category={el.type}
+              >
+                {el.title}
+              </GbCard50>
+            ))}
+          </div>
+        ) : (
+          <div
+            style={{ display: showModal ? "none" : "block" }}
+            className="no-jobs"
+          >
+            No jobs in this category.
+          </div>
+        )}
+      </div>
+    )}
+  </WithModal>
 );
--- src/contents/company/my-jobs/MyJobOffers.js
+++ src/contents/company/my-jobs/MyJobOffers.js
@@ -1,24 +1,21 @@
 import React from "react";
-import fire from '../../../config/Fire';
-import {Link} from 'react-router-dom';
-import {GbCard50} from '../../../components/gbCard50';
+import fire from "../../../config/Fire";
+import { Link } from "react-router-dom";
+import { GbCard50 } from "../../../components/gbCard50";
 import LoadingPage from "../../../components/LoadingPage";
-import {MyJobsCategoryView} from "../../../components/my-jobs/MyJobsCategoryView";
+import { MyJobsCategoryView } from "../../../components/my-jobs/MyJobsCategoryView";
 
 export default class MyJobOffers extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {
-          this.props.loading === false ? (
-            <MyJobOffersFetch {...this.props} />
-          ) : (
-            <LoadingPage/>
-          )
-
-        }
+        {this.props.loading === false ? (
+          <MyJobOffersFetch {...this.props} />
+        ) : (
+          <LoadingPage />
+        )}
       </React.Fragment>
-    )
+    );
   }
 }
 
@@ -29,72 +26,90 @@ class MyJobOffersFetch extends React.Component {
     jobsList: [],
     openJobs: [],
     inProgressJobs: [],
-    closedJobs: [],
+    closedJobs: []
   };
 
   componentDidMount() {
     /**
      * Check if there is a user on and if it is a company
      **/
-    if (!this.props.user || this.props.user.type !== 'company') {
-      this.props.history.replace('/');
+    if (!this.props.user || this.props.user.type !== "company") {
+      this.props.history.replace("/");
     } else {
       //First get the data about the current company which jobs they have
-      fire.database()
-        .ref('company')
+      fire
+        .database()
+        .ref("company")
         .child(this.props.user.uid)
-        .once('value', (snap) => {
+        .once("value", snap => {
           /*After having the keys of the jobs go ahead and get the data about them */
           let jobsIds = Object.keys(snap.val().postedJobs || {});
 
           if (jobsIds.length === 0) {
             this.setState({
               stage: 2,
-              loadingDb: false,
-            })
+              loadingDb: false
+            });
           } else {
             this.setState({
-              stage: 1,
+              stage: 1
             });
-            jobsIds = jobsIds.map(el => fire.database().ref('requests').child(el).once('value'));
-            Promise.all(jobsIds)
-              .then((values) => {
-                let allJobs = values.map(el => el.val());
-                this.setState({
-                  loadingDb: false,
-                  stage: 2,
-                  jobsList: values.map(el => el.val()),
-                  openJobs: allJobs.filter(job => job.status === "open"),
-                  inProgressJobs: allJobs.filter(job => job.status === "in progress"),
-                  closedJobs: allJobs.filter(job => job.status === "closed"),
-                });
+            jobsIds = jobsIds.map(el =>
+              fire
+                .database()
+                .ref("requests")
+                .child(el)
+                .once("value")
+            );
+            Promise.all(jobsIds).then(values => {
+              let allJobs = values.map(el => el.val());
+              this.setState({
+                loadingDb: false,
+                stage: 2,
+                jobsList: values.map(el => el.val()),
+                openJobs: allJobs.filter(job => job.status === "open"),
+                inProgressJobs: allJobs.filter(
+                  job => job.status === "in progress"
+                ),
+                closedJobs: allJobs.filter(job => job.status === "closed")
               });
+            });
           }
         });
     }
   }
 
   render() {
-    const {loadingDb, stage, jobsList} = this.state;
+    const { loadingDb, stage, jobsList } = this.state;
     return (
       <React.Fragment>
-        {
-          loadingDb ? (
-            <h2>Loading db... stage {stage}/2</h2>
-          ) : (
-            jobsList.length === 0 ? (
-              <h2>You have no jobs posted yet, create your first <Link to='/createJob'>here</Link></h2>
-            ) : (
-              <div className="my-jobs-container">
-                <MyJobsCategoryView categoryTitle="Open" jobs={this.state.openJobs}/>
-                <MyJobsCategoryView categoryTitle="In Progress" jobs={this.state.inProgressJobs}/>
-                <MyJobsCategoryView categoryTitle="Closed" jobs={this.state.closedJobs}/>
-              </div>
-            )
-
-          )
-        }
+        {loadingDb ? (
+          <h2>
+            Loading db... stage {stage}
+            /2
+          </h2>
+        ) : jobsList.length === 0 ? (
+          <h2>
+            You have no jobs posted yet, create your first{" "}
+            <Link to="/createJob">here</Link>
+          </h2>
+        ) : (
+          <div className="my-jobs-container">
+            <MyJobsCategoryView
+              categoryTitle="Open"
+              jobs={this.state.openJobs}
+            />
+            <MyJobsCategoryView
+              categoryTitle="In Progress"
+              jobs={this.state.inProgressJobs}
+            />
+            <MyJobsCategoryView
+              categoryTitle="Closed"
+              jobs={this.state.closedJobs}
+            />
+          </div>
+        )}
       </React.Fragment>
-    )
+    );
   }
 }
