28a87e97f372f1182a89631a6f747bd6f5b542e1
Bianca <bianca.hoellmueller@gmx.net>
2018-12-04 15:10:09 +0100

    created ReviewBox component

--- src/contents/photographer/dashboard/PhotographerContent.js
+++ src/contents/photographer/dashboard/PhotographerContent.js
@@ -2,9 +2,8 @@ import React from "react";
 import { PortofolioGallery } from "../../../components/PortofolioGallery";
 import { PropTypes } from "prop-types";
 import {HireButton} from "../../../components/HireButton";
-import {Avatar} from "../../../components/label/Avatar";
-import {ArrowSVG} from "../../../components/svg/ArrowSVG";
 import {Link} from "react-router-dom";
+import {ReviewBox} from "../../../components/profile/ReviewBox";
 
 export const PhotographerContent = ({ photographerData, isOtherUser, siggnedInUser, uid, finishedJobs, hireable }) => (
   <React.Fragment>
@@ -39,14 +38,11 @@ export const PhotographerContent = ({ photographerData, isOtherUser, siggnedInUs
         />
       </div>
     }
-    <div className="review-box">
-      <h2>Last review</h2>
-      <Avatar userImageUrl="https://images.askmen.com/1080x540/2016/01/25-021526-facebook_profile_picture_affects_chances_of_getting_hired.jpg"/>
-      <h3>John Doe</h3>
-      <p>“New battery for smartphones can
-        be charged in a minute”</p>
-      <ArrowSVG classes="gb-icon-medium"/>
-    </div>
+    <ReviewBox title="Last review"
+               userImageURL="https://images.askmen.com/1080x540/2016/01/25-021526-facebook_profile_picture_affects_chances_of_getting_hired.jpg"
+               name="John Doe"
+               quote="New battery for smartphones can be charged in a minute"
+    />
   </React.Fragment>
 );
 
