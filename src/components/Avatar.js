7c6c1e2039b93f2407cfa16f35466f019bbe20fc
Bianca <bianca.hoellmueller@gmx.net>
2018-12-04 14:52:43 +0100

    implemented photographer profile design

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {StarSVG} from "./svg/StarSVG";
 
-export const Avatar = ({ userImageUrl, classes, profileLink }) => (
+export const Avatar = ({ userImageUrl, classes, profileLink, premium }) => (
   <a className="profile-link" href={profileLink}>
+    {premium && <div className="premium-flag"><StarSVG classes="gb-icon-medium gb-icon-fill-white"/></div>}
     <img src={userImageUrl} alt="avatar" className={`gb-avatar ${classes}`} />
   </a>
 );
